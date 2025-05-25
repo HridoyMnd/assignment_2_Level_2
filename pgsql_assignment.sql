cd37ed9370727b5b9bfc25285045930db6ddb92e
Hridoy Mnd <hridoymnd10@gmail.com>
2025-05-25 20:04:19 +0600

    species table create and data insert

--- pgsql_assignment.sql
+++ pgsql_assignment.sql
@@ -25,3 +25,36 @@ INSERT INTO rangers (ranger_name, ranger_region) VALUES
 ('Nisha Verma', 'Manas National Park'),
 ('Rohit Roy', 'Periyar'),
 ('Anjali Mehra', 'Ranthambore')
+
+
+-- create species  table
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name TEXT,
+    scientific_name TEXT,
+    discovery_date DATE,
+    conservation_status VARCHAR(50)
+);
+
+
+-- updata type date to timestamp
+ALTER TABLE sightings
+    ALTER COLUMN sighting_time TYPE TIMESTAMP;
+
+
+-- Provided data insert into species 
+INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status) VALUES
+('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+('Bengal Tiger', 'Panthera tigris', '1758-01-01', 'Endangered'),
+('Red Panda', 'Ailurus fulgens ', '1825-01-01', 'Vulnerable'),
+('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered')
+
+
+-- extra data insert into species
+INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status) VALUES
+('Indian Rhinoceros', 'Rhinoceros unicornis', '1758-01-01', 'Vulnerable'),
+('Asiatic Lion', 'Panthera leo persica', '1826-01-01', 'Endangered'),
+('Spotted Deer', 'Axis axis', '1777-01-01', 'Vulnerable'),
+('Sloth Bear', 'Melursus ursinus', '1791-01-01', 'Vulnerable'),
+('Indian Peacock', 'Pavo cristatus', '1758-01-01', 'Endangered'),
+('Great Indian Bustard', 'Ardeotis nigriceps', '1863-01-01', 'Endangered')
