a2449d9a65b1f423de4919bee72caea227a4878b
Hridoy Mnd <hridoymnd10@gmail.com>
2025-05-25 19:58:35 +0600

    create ranger table and data insert

--- pgsql_assignment.sql
+++ pgsql_assignment.sql
@@ -1,13 +1,27 @@
 -- Active: 1747547619109@@127.0.0.1@5432@pgsql_ass_db
-CREATE TABLE test (
-    id SERIAL PRIMARY KEY,
-    name TEXT
-);
 
-INSERT INTO test (name) VALUES
-('hridoy'),
-('sagor'),
-('batach'),
-('akash')
+-- Create ranger table
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    ranger_name TEXT,
+    ranger_region TEXT
+)
 
-SELECT * FROM test
+
+-- Provided data insert into rangers
+INSERT INTO rangers(ranger_name, ranger_region) VALUES 
+('Alice Green', 'Northern Hills'),
+('Bob White', 'River Delta'),
+('Carol King', 'Mountain Range')
+
+
+-- extra data insert into rangers
+INSERT INTO rangers (ranger_name, ranger_region) VALUES
+('Ajay Kumar', 'Sundarbans'),
+('Reena Das', 'Kaziranga'),
+('Sanjay Singh', 'Bandipur'),
+('Meera Patel', 'Gir Forest'),
+('Vikram Sen', 'Jim Corbett'),
+('Nisha Verma', 'Manas National Park'),
+('Rohit Roy', 'Periyar'),
+('Anjali Mehra', 'Ranthambore')
